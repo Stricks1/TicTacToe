16e2f5a7b93b6b1676bd662dd907086f5fd1bcc7
Stricks1 <gmalheiross@gmail.com>
2020-05-18 16:59:40 -0300

    deleted files not changed on this branch

--- lib/playagain.rb
+++
@@ -1,7 +0,0 @@
-def play_again(input)
-  if %w[Y y].include?(input)
-    true
-  else
-    false
-  end
-end
--- lib/player.rb
+++
@@ -1,8 +0,0 @@
-class Player
-  attr_reader :character, :player
-
-  def initialize(character, player)
-    @character = character
-    @player = player
-  end
-end
